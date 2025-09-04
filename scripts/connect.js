c136b3d8c5b307c71be70f131ea95aa08f34c20f
EricStratosphere <140722924+EricStratosphere@users.noreply.github.com>
2025-09-04 11:30:12 +0800

    Integrate footer.html into index.html

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,11 +1,15 @@
 import loadCardComponent from "./card-component.js";
-
+import loadFooterComponent from "./footer.js";
 const body = document.body;
 
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const cardComponent = await loadCardComponent();
-  const body = document.getElementById('box-element');
-  body.appendChild(cardComponent);
+    const body = document.getElementById('box-element');
+    const cardComponent = await loadCardComponent();
+  
+    body.appendChild(cardComponent);
+
+    const footerComponent = await loadFooterComponent();
+    body.appendChild(footerComponent);
 });
 
